2bf3894238341ad637a3e278ccd45402bb5b06d3
gedeontimothy <gedeonbateko3@gmail.com>
2026-04-29 00:39:21 +0100

    [ADD] Component - Added the `AppText` component
    A custom variant of React Native's `Text`, modified according to the application.

---
+++ src/components/AppText.js
@@ -0,0 +1,23 @@
+import { Text } from 'react-native';
+
+import { Fonts } from '../theme/fonts';
+import spacing, { scaledFontSize } from '../theme/spacing';
+import baseColor from '../theme/color';
+
+export default function AppText({children, color, scaled = false, size, weight, style, ...props}){
+	return (
+		<Text
+			style={[
+				{
+					color: baseColor[color] ?? color,
+					fontFamily: Fonts[weight] ?? Fonts.regular,
+					fontSize: scaled ? scaledFontSize(size) : (spacing.fontSize[size] ?? spacing.fontSize.base),
+				},
+				style,
+			]}
+			{...props}
+		>
+			{children}
+		</Text>
+	)
+}
