3c345610e43dccff25d122975739b9e02b7b7bb7
gedeontimothy <gedeonbateko3@gmail.com>
2026-05-09 22:50:10 +0100

    [ADD|UPDATE] Config | Internationalisation - Adding and configuring internationalization for English and French languages

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import 'intl-pluralrules';
 import 'react-native-gesture-handler';
+import {AppRegistry} from 'react-native';
 
 import App from './App';
 
---
+++ src/config/i18n.js
@@ -0,0 +1,39 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+
+import { store } from '../store';
+
+import en from '../locales/en.json';
+import fr from '../locales/fr.json';
+
+let initialized = false;
+
+export async function initI18n(language) {
+	if (initialized) return;
+
+	await i18n
+		.use(initReactI18next)
+		.init({
+			resources: {
+				en: {translation: en},
+				fr: {translation: fr},
+			},
+			lng: language,
+			fallbackLng: 'en',
+			interpolation: {
+				escapeValue: false,
+			},
+			react: {
+				useSuspense: false,
+			},
+		});
+		
+		store.subscribe(() => {
+			const currentLanguage = store.getState().lang.currentLanguage;
+			i18n.changeLanguage(currentLanguage);
+		});
+
+	initialized = true;
+}
+
+export default i18n;
