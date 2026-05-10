973c7a9382f1f3c06a6ce9e254e3aaea64b5449e
gedeontimothy <gedeonbateko3@gmail.com>
2026-05-10 03:07:31 +0100

    [UPDATE] Provider - Update `RootProvider`
    - Setting up the Redux store via its provider
    - Added change listening to `reduceMotionChanged` accessibility
    - Language initialization via the store

--- src/providers/root.provider.js
+++ src/providers/root.provider.js
@@ -1,15 +1,35 @@
+import { AccessibilityInfo } from 'react-native';
 import React, {useEffect, useState} from 'react'
+import { Provider, useDispatch } from 'react-redux';
+
+import {store} from '../store'
+import { setAnimate } from '../store/features/settings/settings.slice';
+
+import { initI18n } from '../config/i18n';
 
 import BootLoadingScreen from '../screens/boot.loading.screen'
-
+import { initLang } from '../store/features/lang/lang.slice';
 
 const Boot = ({children}) => {
 	const [loading, setLoading] = useState(true);
 
+	const dispatch = useDispatch();
+
 	useEffect(() => {
-		setTimeout(() => {
-			setLoading(false);
-		}, 3000);
+		const call = async () => {
+			await accessibilityEventListenerThunk(dispatch);
+
+			await initI18n(store.getState().lang.currentLanguage);
+
+			await dispatch(initLang());
+
+			setTimeout(() => {
+				setLoading(false);
+			}, 500)
+		}
+
+		call();
+
 	}, []);
 
 	if(loading) return <BootLoadingScreen/>;
@@ -19,8 +39,19 @@ const Boot = ({children}) => {
 
 export default ({children}) => {
 	return (
-		<Boot>
-			{children}
-		</Boot>
+		<Provider store={store}>
+			<Boot>
+				{children}
+			</Boot>
+		</Provider>
 	)
 }
+
+async function accessibilityEventListenerThunk(dispatch) {
+
+	AccessibilityInfo.addEventListener(
+		'reduceMotionChanged',
+		async () => dispatch(setAnimate(!await AccessibilityInfo.isReduceMotionEnabled()))
+	);
+
+};
