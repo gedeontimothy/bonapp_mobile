bfbae2dab6def23716f60c2bf6f88c50b39356b2
gedeontimothy <gedeonbateko3@gmail.com>
2026-05-11 18:28:06 +0100

    [ADD|UPDATE] Store | Features | Setting - Update `settings` store features
    - Added selectors
    - Added reducers for themes and setting processes

---
+++ src/store/features/settings/settings.selector.js
@@ -0,0 +1,46 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { buildProcessSelectors } from "../process/process.selector";
+
+/**
+ * Retrieves the current theme select
+ *
+ * @param {object} state - The Redux state
+ * @returns {string}
+ */
+export const currentTheme = (state) => state.settings.theme.current;
+
+/**
+ * Returns the list of available themes
+ *
+ * @param {object} state - The Redux state
+ * @returns {string[]}
+ */
+export const availableThemes = (state) => state.settings.theme.availableThemes;
+
+/**
+ * Returns the list of available themes excluding the current theme
+ *
+ * @var {string[]}
+ */
+export const otherAvailableThemes = createSelector(
+	[availableThemes, currentTheme],
+	(themes, current) => themes.filter(theme => theme !== current)
+);
+
+/**
+ * Check if any theme is current theme.
+ *
+ * @var {boolean}
+ */
+export const isCurrentTheme = (theme) => createSelector(
+	[currentTheme],
+	(current_theme) => theme == current_theme
+);
+
+export const {
+	getProcess: getSettingProcess,
+	getProcesses: getSettingProcesses,
+	getProcessesByActionType: getSettingProcessesByActionType,
+	countPendingProcess: countSettingPendingProcess,
+	countPendingProcessByActionType : countPendingSettingProcessByActionType,
+} = buildProcessSelectors("settings")
