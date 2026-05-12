bc10d47a1db9c1a90fe15443e630656db30ee62c
gedeontimothy <gedeonbateko3@gmail.com>
2026-05-12 23:04:48 +0100

    [UPDATE] Screen - Update `WelcomeScreen`

--- src/screens/guest/welcome.screen.js
+++ src/screens/guest/welcome.screen.js
@@ -2,102 +2,97 @@ import {
 	View,
 	StyleSheet,
 	SafeAreaView,
+	ScrollView,
 } from 'react-native';
-import { useTranslation } from 'react-i18next';
-import {Picker} from '@react-native-picker/picker';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
 import color from '../../theme/color';
 
-import { useTheme } from '../../hooks/theme';
-
 import AppText from '../../components/AppText';
-import { Fonts } from '../../theme/fonts';
-import { useLanguage } from '../../hooks/lang';
+import BarLayout from '../../layouts/bar.layout';
+import spacing, {pixelRatio as pixelRatioSpacing} from '../../theme/spacing';
+import {Button} from '../../components/Button';
 
 export default function WelcomeScreen({navigation}) {
 
-	const { t } = useTranslation();
+	const pixelRatio = pixelRatioSpacing()
 
-	const {
-		changeTheme,
-		changeThemeLoading,
-		colorScheme,
-		currentTheme : current_theme,
-		themes
-	} = useTheme();
-
-	const {
-		changeLanguage,
-		changeLanguageLoading,
-		currentLanguage : current_language,
-		languages
-	} = useLanguage();
-
-	const handleSelectChangeLanguage = (itemValue, itemIndex) => {
-		if(itemValue != current_language) {
-			changeLanguage(itemValue)
-		}
-	}
-
-	const handleSelectChangeTheme = (itemValue, itemIndex) => {
-		if(itemValue != current_theme) {
-			changeTheme(itemValue)
-		}
-	}
-
+	const AppTextScaled = ({...props}) => <AppText scaled={true} {...props}/>
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<AppText size="3xl" scaled={true} color="black">{t('welcome')}</AppText>
-			<View style={styles.langContainer}>
-				<AppText scaled={true} size="xl" color="black">{t("lang.base")}</AppText>
-				<View style={styles.pickerWrapper}>
-					<Picker
-						enabled={!changeLanguageLoading}
-						selectedValue={current_language}
-						onValueChange={handleSelectChangeLanguage}
-						style={styles.picker}
-						dropdownIconColor={color["secondary-light-400"]}
-					>
-						{languages.map(lang => {
-							return (
-								<Picker.Item
-									style={styles.pickerItem}
-									key={lang}
-									label={t("lang." + lang)}
-									value={lang}
-								/>
-							)
-						})}
-					</Picker>
+			<BarLayout/>
+			<ScrollView contentContainerStyle={styles.scrollContainer}>
+
+				{/* ----- HEADER CONTENT ----- */}
+				<View style={styles.headerContainer}>
+					<View style={styles.brandContainer}>
+						<View style={styles.logo}>
+							<Icon
+								name="diamond"
+								size={24 * pixelRatio}
+								color={color.primary}
+							/>
+						</View>
+						<AppTextScaled
+							style={styles.brandText}
+							font='medium'
+							color="primary"
+							size="3xl"
+						>BONAPP</AppTextScaled>
+					</View>
 				</View>
-			</View>
-			<View style={styles.langContainer}>
-				<AppText scaled={true} size="xl" color="black">{t("theme.base")}</AppText>
-				<View style={styles.pickerWrapper}>
-					<Picker
-						enabled={!changeThemeLoading}
-						selectedValue={current_theme}
-						onValueChange={handleSelectChangeTheme}
-						style={styles.picker}
-						dropdownIconColor={color["secondary-light-400"]}
-					>
-						{themes.map(theme => {
-							return (
-								<Picker.Item
-									style={styles.pickerItem}
-									key={theme}
-									label={t("theme." + theme) + (theme == 'system' 
-										? " (" + t("theme." + colorScheme) + ")"
-										: ""
-									)}
-									value={theme}
-								/>
-							)
-						})}
-					</Picker>
+
+
+
+				{/* ----- BODY CONTENT ----- */}
+				<View style={styles.bodyContainer}>
+					<View>
+						<AppTextScaled
+							style={styles.captionSlogan}
+							font='regular'
+							color="primary"
+						>L'EXCELENCE FINANCIÈRE</AppTextScaled>
+						<AppTextScaled
+							style={[styles.captionTitle, {lineHeight: (4 + spacing.fontSize["6xl"]) * pixelRatio}]}
+							font='semibold'
+							color="black"
+							size="6xl"
+						>Bienvenue dans l'Atelier Financier.</AppTextScaled>
+						<AppTextScaled
+							style={styles.captionDescription}
+							font='light'
+							color="black"
+							size="2xl"
+						>Gérez vos bons de sortie de caisse avec une précision absolue. Une solution dédiée au suivi rigoureux de vos flux et retraits d'argent.</AppTextScaled>
+					</View>
+					<Button
+						style={styles.button}
+						textProps={{
+							font: "medium",
+							size: "xl",
+							style: {marginRight: 12},
+							scaled: true
+						}}
+						rightContent={
+							<Icon name="arrow-right-circle" size={spacing.fontSize["xl"] * pixelRatio} color="white"/>
+						}
+					>Commencer l'aventure</Button>
 				</View>
-			</View>
+
+
+
+				{/* ----- FOOT CONTENT ----- */}
+				<View style={styles.footContainer}>
+					<View style={styles.line}/>
+					<View style={styles.terms}>
+						<AppTextScaled style={{marginRight: 28}} color='neutral-dark-600' font='medium'>CONFIDENTIALITÉ</AppTextScaled>
+						<AppTextScaled color='neutral-dark-600' font='medium'>CONDITIONS</AppTextScaled>
+					</View>
+					<AppTextScaled color='neutral-dark-600' font='medium'>© 2024 BONAPP ATELIER</AppTextScaled>
+				</View>
+
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
@@ -105,27 +100,64 @@ export default function WelcomeScreen({navigation}) {
 const styles = StyleSheet.create({
 	container: {
 		flex:1,
+		backgroundColor: "white",
 	},
-	langContainer: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-		paddingHorizontal: 20,
-		paddingVertical: 10,
-		backgroundColor: color["secondary-light-100"]
+	scrollContainer: {
+		flexGrow: 1,
+		flexDirection: "column",
 	},
-	pickerWrapper: {
-		width: '50%',
-		borderWidth: 2,
-		borderColor: color["secondary-light-200"],
-		borderRadius: 16,
-	},
-	picker: {
-		width: '100%',
-		color: "black",
-	},
-	pickerItem: { 
-		fontFamily: Fonts.regular, 
-		// color: "black",
-	},
+
+	// HEADER
+		headerContainer: {
+			marginTop: 36,
+			paddingHorizontal: 24,
+		},
+		brandContainer: {
+			flexDirection: "row",
+			alignItems: "center",
+		},
+		brandText: {
+			letterSpacing: 3,
+		},
+		logo: {
+			marginRight: 8,
+		},
+
+
+
+	// BODY
+		bodyContainer: {
+			flexGrow: 1,
+			marginVertical: 48,
+			paddingHorizontal: 24,
+		},
+		captionSlogan: {
+			letterSpacing: 3,
+		},
+		captionTitle: {
+			marginTop: 16,
+		},
+		captionDescription: {
+			marginTop: 20,
+		},
+		button: {
+			marginTop: 48,
+		},
+
+
+	// FOOTER
+		footContainer: {
+			paddingHorizontal: 24,
+			paddingBottom: 24,
+		},
+		terms: {
+			flexDirection: "row",
+			marginBottom: 16,
+		},
+		line: {
+			backgroundColor: color['primary-light'],
+			height: 1,
+			borderRadius: 2,
+			marginBottom: 36,
+		},
 });
