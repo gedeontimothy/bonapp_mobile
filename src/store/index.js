8356ca51bb50998874f43c122c26dcbe103fcaff
gedeontimothy <gedeonbateko3@gmail.com>
2026-05-31 03:23:21 +0100

    [UPDATE] Store - Added `auth` reducer slice in store

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,13 @@ import langReducer from './features/lang/lang.slice';
 import settingsReducer from './features/settings/settings.slice';
 import processReducer from './features/process/process.slice';
 import usersReducer from './features/users/users.slice';
+import authReducer from './features/auth/auth.slice';
 
 const rootReducer = combineReducers({
 	settings: settingsReducer,
 	lang: langReducer,
 	process: processReducer,
+	auth: authReducer,
 
 	users: usersReducer,
 });
